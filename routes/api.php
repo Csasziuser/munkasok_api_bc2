f54382bb62bcd8debdca116e5fd1eaca8adc9df0
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2026-02-27 08:51:31 +0100

    kesz a route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KijelolesController;
+use App\Http\Controllers\MunkasController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('munkasok', [MunkasController::class, "index"]);
+Route::post('munkasok', [MunkasController::class, "store"]);
+Route::put('munkasok', [MunkasController::class, "update"]);
+
+Route::get('kijelolesek', [KijelolesController::class, "index"]);
+Route::post('kijelolesek', [KijelolesController::class, "store"]);
